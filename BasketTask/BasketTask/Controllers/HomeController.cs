ca0b5da2a7c3f29a7e0e22e05a154778b95dad13
Said Mikailov <saidgm@code.edu.az>
2022-03-23 21:57:26 +0400

    Homework

---
+++ BasketTask/BasketTask/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BasketTask.Data;
+using BasketTask.Models;
+using BasketTask.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.Include(i=>i.Images).ToListAsync();
+            SliderImage sliderImage = await _context.SliderImages.FirstOrDefaultAsync();
+            List<Categories> categories = await _context.Categories.Include(i => i.CategoryProducts).ToListAsync();
+            List<CategoryProducts> categoryProducts = await _context.CategoryProducts.ToListAsync();
+
+            HomeVM homeVM = new HomeVM
+            {
+                Sliders = sliders,
+                SliderImage = sliderImage,
+                Categories = categories,
+                CategoryProducts = categoryProducts
+            };
+
+            return View(homeVM);
+        }
+    }
+}
---
+++ BasketTask/BasketTask/ViewModels/HomeVM.cs
@@ -0,0 +1,16 @@
+﻿using BasketTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketTask.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+        public SliderImage SliderImage { get; set; }
+        public List<Categories> Categories { get; set; }
+        public List<CategoryProducts> CategoryProducts { get; set; }
+    }
+}
